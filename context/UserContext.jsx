789f31fb4620d59974cd53160e5ed15f6cc244d5
Azra Javed <azra.javed.dev@gmail.com>
2026-07-28 19:03:28 +0500

    initial auth state

--- context/UserContext.jsx
+++ context/UserContext.jsx
@@ -1,10 +1,11 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { account } from "../lib/appwrite";
 import { ID } from "react-native-appwrite";
 export const userContext = createContext();
 
 export default function UserProvider({ children }) {
   const [user, setUser] = useState(null);
+  const [authChecked, setAuthChecked] = useState(false);
 
   async function login(email, password) {
     try {
@@ -30,8 +31,24 @@ export default function UserProvider({ children }) {
     setUser(null);
   }
 
+  async function getInitialUserValue() {
+    try {
+      const response = await account.get();
+      setUser(response);
+    } catch (error) {
+      setUser(null);
+    } finally {
+      setAuthChecked(true);
+    }
+  }
+  useEffect(() => {
+    getInitialUserValue();
+  }, []);
+
   return (
-    <userContext.Provider value={{ user, login, register, logout }}>
+    <userContext.Provider
+      value={{ user, login, register, logout, authChecked }}
+    >
       {children}
     </userContext.Provider>
   );
