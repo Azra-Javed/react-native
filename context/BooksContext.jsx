a28c44e69b106df8a12030409e57ca9666b9dcc1
Azra Javed <azra.javed.dev@gmail.com>
2026-07-29 17:38:51 +0500

    fetching book records

--- context/BooksContext.jsx
+++ context/BooksContext.jsx
@@ -1,6 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { databases } from "../lib/appwrite";
-import { ID, Permission, Role } from "react-native-appwrite";
+import { ID, Permission, Query, Role } from "react-native-appwrite";
 import { useUser } from "../hooks/useUser";
 
 const DATABASE_ID = "6a696afd000d2f8406b2";
@@ -12,6 +12,14 @@ export function BooksProvider({ children }) {
 
   async function fetchBooks() {
     try {
+      const response = await databases.listDocuments(
+        DATABASE_ID,
+        COLLECTION_ID,
+        [Query.equal("userId", user.$id)],
+      );
+
+      setBooks(response.documenst);
+      console.log(response.documents);
     } catch (error) {
       console.error(error.message);
     }
@@ -54,6 +62,12 @@ export function BooksProvider({ children }) {
     }
   }
 
+  useEffect(() => {
+    if (user) fetchBooks();
+    else {
+      setBooks([]);
+    }
+  }, [user]);
   return (
     <BooksContext.Provider
       value={{
