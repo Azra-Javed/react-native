424b0e63a3b1566e2a531f99a96bdee82dcbf5ae
Azra Javed <azra.javed.dev@gmail.com>
2026-07-29 09:36:20 +0500

    add functionality to create books

--- app/(dashboard)/create.jsx
+++ app/(dashboard)/create.jsx
@@ -1,16 +1,86 @@
-import { StyleSheet } from "react-native";
+import { router, useRouter } from "expo-router";
+import { useState } from "react";
+import {
+  Keyboard,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+} from "react-native";
 import Spacer from "../../components/Spacer";
+import ThemedButton from "../../components/ThemedButton";
 import ThemedText from "../../components/ThemedText";
+import ThemedTextInput from "../../components/ThemedTextInput";
 import ThemedView from "../../components/ThemedView";
+import { useBooks } from "../../hooks/useBooks";
 
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [description, setDescription] = useState("");
+
+  const [loading, setLoading] = useState(false);
+
+  const { createBook } = useBooks();
+  const router = useRouter();
+
+  const handleSubmit = async () => {
+    if (!title.trim() || !author.trim() || !description.trim()) return;
+    setLoading(true);
+
+    await createBook({ title, author, description });
+
+    //reset fileds
+    setTitle("");
+    setAuthor("");
+    setDescription("");
+
+    //redirect
+    router.replace("/books");
+
+    //reset laoding state
+    setLoading(false);
+  };
+
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText title={true} style={styles.heading}>
-        Add new Book
-      </ThemedText>
-      <Spacer />
-    </ThemedView>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <ThemedView style={styles.container}>
+        <ThemedText title={true} style={styles.heading}>
+          Add a New Book
+        </ThemedText>
+        <Spacer />
+
+        <ThemedTextInput
+          style={styles.input}
+          placeholder="Book Title"
+          value={title}
+          onChangeText={setTitle}
+        />
+        <Spacer />
+
+        <ThemedTextInput
+          style={styles.input}
+          placeholder="Author"
+          value={author}
+          onChangeText={setAuthor}
+        />
+        <Spacer />
+
+        <ThemedTextInput
+          style={styles.multiline}
+          placeholder="Book Description"
+          value={description}
+          onChangeText={setDescription}
+          multiline={true}
+        />
+        <Spacer />
+
+        <ThemedButton onPress={handleSubmit} disabled={loading}>
+          <Text style={{ color: "#fff" }}>
+            {loading ? "Saving..." : "Create Book"}
+          </Text>
+        </ThemedButton>
+      </ThemedView>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -23,4 +93,18 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   heading: { fontWeight: "bold", fontSize: 18, textAlign: "center" },
+
+  input: {
+    padding: 20,
+    borderRadius: 6,
+    alignSelf: "stretch",
+    marginHorizontal: 40,
+  },
+  multiline: {
+    padding: 20,
+    borderRadius: 6,
+    minHeight: 100,
+    alignSelf: "stretch",
+    marginHorizontal: 40,
+  },
 });
--- context/BooksContext.jsx
+++ context/BooksContext.jsx
@@ -1,11 +1,14 @@
 import { createContext, useState } from "react";
+import { databases } from "../lib/appwrite";
+import { ID, Permission, Role } from "react-native-appwrite";
+import { useUser } from "../hooks/useUser";
 
-
-const DATABASE_ID="6a696afd000d2f8406b2"
-const COLLECTION_ID="b"
+const DATABASE_ID = "6a696afd000d2f8406b2";
+const COLLECTION_ID = "b";
 export const BooksContext = createContext();
 export function BooksProvider({ children }) {
   const [books, setBooks] = useState([]);
+  const { user } = useUser();
 
   async function fetchBooks() {
     try {
@@ -22,22 +25,46 @@ export function BooksProvider({ children }) {
   }
 
   async function createBook(data) {
+    if (!user) return;
+
+    try {
+      const newBook = await databases.createDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        ID.unique(),
+        {
+          ...data,
+          userId: user.$id,
+        },
+        [
+          Permission.read(Role.user(user.$id)),
+          Permission.update(Role.user(user.$id)),
+          Permission.delete(Role.user(user.$id)),
+        ],
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function deleteBook(id) {
     try {
     } catch (error) {
       console.error(error.message);
     }
   }
 
-  async function delteBoook(id) {
-    try {
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
-
-  return(
-    <BooksContext.Provider value={books, fetchBooks, fetchBookById, createBook, delteBoook}>
+  return (
+    <BooksContext.Provider
+      value={{
+        books,
+        fetchBooks,
+        fetchBookById,
+        createBook,
+        deleteBook,
+      }}
+    >
       {children}
     </BooksContext.Provider>
-  )
+  );
 }
